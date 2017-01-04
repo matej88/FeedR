62152a01bb1cb48283805a1c9da43a8c5030b639
itsmadinnit <matej.kadric@gmail.com>
2017-01-04 22:35:27 +0100

    commit

--- app/src/main/java/se/chalmers/exjobb/feedr/fragments/CourseOverviewFragment.java
+++ app/src/main/java/se/chalmers/exjobb/feedr/fragments/CourseOverviewFragment.java
@@ -7,9 +7,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import se.chalmers.exjobb.feedr.R;
 import se.chalmers.exjobb.feedr.models.Course;
+import se.chalmers.exjobb.feedr.models.Feedback;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,9 +32,13 @@ public class CourseOverviewFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_COURSE = "course";
     private static final String ARG_COURSEKEY = "courseKey";
+    private static final String FEEDBACKS = "feedbacks";
 
     private Course mCourse;
     private String courseKey;
+
+    private DatabaseReference mDataRef;
+    private DatabaseReference mRatingsRef;
 
 
 
@@ -57,6 +73,8 @@ public class CourseOverviewFragment extends Fragment {
             courseKey = getArguments().getString(ARG_COURSEKEY);
 
         }
+
+
     }
 
     @Override
@@ -64,7 +82,36 @@ public class CourseOverviewFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_course_overview, container, false);
+        final List<Double> ratings = new ArrayList<>();
 
+        mDataRef = FirebaseDatabase.getInstance().getReference();
+        mRatingsRef = mDataRef.child(FEEDBACKS);
+        ratings.clear();
+        mRatingsRef.orderByChild(courseKey).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterator<DataSnapshot> children = dataSnapshot.getChildren().iterator();
+
+                while(children.hasNext()){
+                    DataSnapshot item = children.next();
+                    Feedback feed;
+                    double rating;
+                    feed = item.getValue(Feedback.class);
+                    rating = feed.getRating();
+                    ratings.add(rating);
+
+                }
+
+                Toast.makeText(getActivity(), String.valueOf(ratings.size()), Toast.LENGTH_LONG);
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         TextView courseName = (TextView) view.findViewById(R.id.course_overview_name);
         TextView courseCode = (TextView) view.findViewById(R.id.course_overview_code);
 
